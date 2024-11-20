05218573abcec203eaafc2c383330766004a508d
Merajul Hasan <merajulhasan09@gmail.com>
2024-11-20 10:31:39 +0600

    added loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,10 @@
+import { HashLoader } from "react-spinners";
+const Loader = () => {
+  return (
+    <div className="min-h-screen min-w-full flex justify-center items-center">
+      <HashLoader color="#1084db" />
+    </div>
+  );
+};
+
+export default Loader;
